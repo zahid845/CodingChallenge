6fed393f042c7a2da313d11c349eba6f93d68059
zahid845 <zahidnazir14@gmail.com>
2019-08-31 13:36:14 +0500

    Initial Commit

---
+++ CodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by Zahid Nazir on 31/08/2019.
+//  Copyright © 2019 Zahid Nazir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
